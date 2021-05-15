8d2bbe792dafcd420a4a068b0aaec8bf5a0ac507
Mohammed <mohammedq91@gmail.com>
2021-05-14 20:36:07 -0400

    starting expense track app

---
+++ script.js
@@ -0,0 +1,9 @@
+//var paymentMethod = getElementById("payment method").addEventListener('click', add)
+//var itemName =getElementById("item name").addEventListener('click',add)
+//var date = getElementById("date").addEventListener('click', add)
+//var amount = getElementById("amount").addEventListener('click', add)
+const addNewExpense = getElementById("button").addEventListener('click',add )
+
+function add(){
+
+}
