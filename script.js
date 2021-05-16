595c00a9b9e037513967a969905907b5ff2ecfe7
Mohammed <mohammedq91@gmail.com>
2021-05-16 09:52:06 -0400

    td cells added to tr

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-var paymentMethod = document.getElementById("payment-method")
-// var itemName =getElementById("item name")
-// var date = getElementById("date")
-// var amount = getElementById("amount")
-const addNewExpense = document.getElementById("button").addEventListener('click', add)
+const paymentMethod = document.getElementById("payment-method")
+const itemName = document.getElementById("item-name")
+const date = document.getElementById("date")
+const amount = document.getElementById("amount")
+const addNewExpense = document.getElementById("button").addEventListener('click',add)
 
 function add(){
   const tableBody = document.getElementById('tbody')
@@ -11,9 +11,22 @@ function add(){
 
   const typeCell = document.createElement('td')
   typeCell.textContent = paymentMethod.value;
-
   tableRow.appendChild(typeCell)
-  console.log(paymentMethod.value)
   
+  const nameCell = document.createElement('td')
+  nameCell.textContent = itemName.value;
+  tableRow.appendChild(nameCell)
 
-}   
+  const dateCell = document.createElement('td')
+  dateCell.textContent = date.value;
+  tableRow.appendChild(dateCell)
+
+  const amountCell = document.createElement('td')
+  amountCell.textContent = amount.value;
+  tableRow.appendChild(amountCell)
+
+  itemName.value = ""
+  date.value= ""
+  amount.value = ""
+
+} 
