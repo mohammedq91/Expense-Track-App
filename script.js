8e2bab4fa76160d74529a8608f8c70e847ab0a05
Mohammed <mohammedq91@gmail.com>
2021-05-21 19:45:31 -0400

    input fields verification via condiditonal statement

--- script.js
+++ script.js
@@ -2,31 +2,47 @@ const paymentMethod = document.getElementById("payment-method")
 const itemName = document.getElementById("item-name")
 const date = document.getElementById("date")
 const amount = document.getElementById("amount")
-const addNewExpense = document.getElementById("button").addEventListener('click',add)
 
-function add(){
-  const tableBody = document.getElementById('tbody')
-  const tableRow = document.createElement('tr')
-  tableBody.appendChild(tableRow);
+const addNewExpenseButton = document
+.getElementById("button")
+.addEventListener('click', () => {
+    if (!paymentMethod.value || 
+      !itemName.value || 
+      !date.value || 
+      !amount.value.value 
+    ){
+      //alert("Please fill out all fields!")
+      console.log('You clicked me!')
+    } else {
+      add;
+    }
+  });
 
-  const typeCell = document.createElement('td')
-  typeCell.textContent = paymentMethod.value;
-  tableRow.appendChild(typeCell)
-  
-  const nameCell = document.createElement('td')
-  nameCell.textContent = itemName.value;
-  tableRow.appendChild(nameCell)
+  function add(tr){
+    const tableBody = document.getElementById('tbody')
+    const tableRow = document.createElement('tr')
+    tableBody.appendChild(tableRow);
 
-  const dateCell = document.createElement('td')
-  dateCell.textContent = date.value;
-  tableRow.appendChild(dateCell)
+    const typeCell = document.createElement('td')
+    typeCell.textContent = paymentMethod.value;
+    tableRow.appendChild(typeCell)
+    
+    const nameCell = document.createElement('td')
+    nameCell.textContent = itemName.value;
+    tableRow.appendChild(nameCell)
 
-  const amountCell = document.createElement('td')
-  amountCell.textContent = amount.value;
-  tableRow.appendChild(amountCell)
+    const dateCell = document.createElement('td')
+    dateCell.textContent = date.value;
+    tableRow.appendChild(dateCell)
 
-  itemName.value = ""
-  date.value= ""
-  amount.value = ""
+    const amountCell = document.createElement('td')
+    amountCell.textContent = amount.value;
+    tableRow.appendChild(amountCell)
 
-} 
+    itemName.value = ""
+    date.value= ""
+    amount.value = ""
+  } 
+
+
+
