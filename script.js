1a591546a1f9a8c0fef9bb94fe46db5e9ce6781e
Mohammed <mohammedq91@gmail.com>
2021-05-21 19:59:37 -0400

    organizing conditional statment

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ const date = document.getElementById("date")
 const amount = document.getElementById("amount")
 
 const addNewExpenseButton = document
-.getElementById("button")
-.addEventListener('click', () => {
+  .getElementById("button")
+  .addEventListener('click', () => {
     if (!paymentMethod.value || 
       !itemName.value || 
       !date.value || 
@@ -14,35 +14,35 @@ const addNewExpenseButton = document
       //alert("Please fill out all fields!")
       console.log('You clicked me!')
     } else {
-      add;
+      add(paymentMethod,itemName, date, amount);
     }
   });
 
-  function add(tr){
-    const tableBody = document.getElementById('tbody')
-    const tableRow = document.createElement('tr')
-    tableBody.appendChild(tableRow);
+function add(tr){
+  const tableBody = document.getElementById('tbody');
+  const tableRow = document.createElement('tr');
+  tableBody.appendChild(tableRow)
 
-    const typeCell = document.createElement('td')
-    typeCell.textContent = paymentMethod.value;
-    tableRow.appendChild(typeCell)
+  const typeCell = document.createElement('td');
+  typeCell.textContent = paymentMethod.value;
+  tableRow.appendChild(typeCell)
     
-    const nameCell = document.createElement('td')
-    nameCell.textContent = itemName.value;
-    tableRow.appendChild(nameCell)
+  const nameCell = document.createElement('td');
+  nameCell.textContent = itemName.value;
+  tableRow.appendChild(nameCell)
 
-    const dateCell = document.createElement('td')
-    dateCell.textContent = date.value;
-    tableRow.appendChild(dateCell)
+  const dateCell = document.createElement('td');
+  dateCell.textContent = date.value;
+  tableRow.appendChild(dateCell)
 
-    const amountCell = document.createElement('td')
-    amountCell.textContent = amount.value;
-    tableRow.appendChild(amountCell)
+  const amountCell = document.createElement('td');
+  amountCell.textContent = amount.value;
+  tableRow.appendChild(amountCell)
 
-    itemName.value = ""
-    date.value= ""
-    amount.value = ""
-  } 
+  itemName.value = ""
+  date.value= ""
+  amount.value = ""
+};
 
 
 
